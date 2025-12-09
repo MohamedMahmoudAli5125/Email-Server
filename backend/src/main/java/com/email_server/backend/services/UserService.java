f1f9900f3e7269479209934fb79d1c89216eb582
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2025-12-09 18:03:16 +0200

    Update UserService.java

--- backend/src/main/java/com/email_server/backend/services/UserService.java
+++ backend/src/main/java/com/email_server/backend/services/UserService.java
@@ -7,80 +7,68 @@
 //import Email_server.Backend.Entities.User;
 //import Email_server.Backend.Repositories.UserRepository;
 //import Email_server.Backend.patterns.Factory;
-//
-//@Service
-//public class UserService {
-//     private final UserRepository userRepository;
-//
-//    public UserService(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-//    // chenck not exist before
-//    // build user
-//    // in user in dto we make @Email which is validation
-//    @Transactional
-//    public User registerUser(UserDTO userDTO) {
-//        if (userRepository.existsByEmail(userDTO.getEmail())) {
-//            throw new RuntimeException("Email already exists");
-//        }
-//        // we make users without decode the passwords
-//        User user = User.builder()
-//                .email(userDTO.getEmail())
-//                .name(userDTO.getName())
-//                .password(userDTO.getPassword())
-//                .build();
-//        // inbox trash ..............
-//        Factory.createDefaultFolders(user);
-//
-//        return userRepository.save(user);
-//    }
-//    // login first
-//    public User login(String email, String password) {
-//        User user = userRepository.findByEmail(email)
-//                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
-//
-//        if (!user.getPassword().equals(password)) {
-//            throw new RuntimeException("Invalid credentials");
-//        }
-//
-//        return user;
-//    }
-//
-//
-//
-//    public User getUserById(String userId) {
-//        return userRepository.findById(userId)
-//                .orElseThrow(() -> new RuntimeException("User not found"));
-//    }
-//
-//    public User getUserByEmail(String email) {
-//        return userRepository.findByEmail(email)
-//                .orElseThrow(() -> new RuntimeException("User not found"));
-//    }
-//
-//
-//    // this also we not need as we not has admin
-//    // public List<User> getAllUsers() {
-//    //     return userRepository.findAll();
-//    // }
-//
-//
-//    // Transactional mean this method make it compact if there is any error happend return the db in orginal before enter the method
-//    // and make open session to db to avoid lazy fetch
-//    // use in methods which make change in db and those changes make combact
-//    // we not need this but can make it as bounus to make user change its data
-//    @Transactional
-//    public User updateUser(String userId, UserDTO userDTO) {
-//        User user = getUserById(userId);
-//        user.setName(userDTO.getName());
-//        return userRepository.save(user);
-//    }
-//    // this also we not need it as we not have admin make delete or ban to users i cancel it
-//    // @Transactional
-//    // public void deleteUser(String userId) {
-//    //     userRepository.deleteById(userId);
-//    // }
-//}
+
+
+// @Service
+// public class UserService {
+//      private final UserRepository userRepository;
+    
+//     public UserService(UserRepository userRepository) {
+//         this.userRepository = userRepository;
+//     }
+//     // chenck not exist before 
+//     // build user 
+//     // in user in dto we make @Email which is validation 
+//     @Transactional
+//     public User registerUser(UserDTO userDTO) {
+//         if (userRepository.existsByEmail(userDTO.getEmail())) {
+//             throw new RuntimeException("Email already exists");
+//         }
+//         // we make users without decode the passwords 
+//         User user = User.builder()
+//                 .email(userDTO.getEmail())
+//                 .name(userDTO.getName())
+//                 .password(userDTO.getPassword()) 
+//                 .build();
+//         // inbox trash ..............
+//         Factory.createDefaultFolders(user);
+        
+//         return userRepository.save(user);
+//     }
+//     // login first 
+//     public User login(String email, String password) {
+//         User user = userRepository.findByEmail(email)
+//                 .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+        
+//         if (!user.getPassword().equals(password)) {
+//             throw new RuntimeException("Invalid credentials");
+//         }
+        
+//         return user;
+//     }
+
+
+    
+//     public User getUserById(String userId) {
+//         return userRepository.findById(userId)
+//                 .orElseThrow(() -> new RuntimeException("User not found"));
+//     }
+    
+//     public User getUserByEmail(String email) {
+//         return userRepository.findByEmail(email)
+//                 .orElseThrow(() -> new RuntimeException("User not found"));
+//     }
+    
+
+//     @Transactional
+//     public User updateUser(String userId, UserDTO userDTO) {
+//         User user = getUserById(userId);
+//         user.setName(userDTO.getName());
+//         return userRepository.save(user);
+//     }
+
+// }
+
 // UserService.java
 package com.email_server.backend.Services;
 
@@ -184,4 +172,5 @@ public class UserService {
         }
         userRepository.deleteById(userId);
     }
+
 }
