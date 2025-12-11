6d6988564866b79d8e0958d3fbd80dfdf8eb8e1e
MohamedMahmoudAli5125 <es.mohamed.mahmoud2023@alexu.edu.eg>
2025-12-11 04:15:31 +0200

    .

--- frontend/app/src/app/shared/nav-bar/nav-bar.ts
+++ frontend/app/src/app/shared/nav-bar/nav-bar.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from '../../models/user';
 import { AuthService } from '../../auth/auth.service';
-
 @Component({
   selector: 'app-nav-bar',
   imports: [],
